0231961fbcd4c3ce36d152cf52968a384ff42b3a
rosa lee <rosalee0116@gmail.com>
2020-05-08 19:15:17 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,4 +17,7 @@ class Scraper
       #course.schedule = post.css(".date").text
       #course.description = post.css("p").text
     end
+    def get_courses
+         self.get_page.css(".post")
+       end
   end
