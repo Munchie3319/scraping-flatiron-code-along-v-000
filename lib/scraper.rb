4b4f619e2384dc0e1e2ce2546407b24e59717ef4
rosa lee <rosalee0116@gmail.com>
2020-05-08 19:20:25 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -33,8 +33,16 @@ class Scraper
   def print_courses
     self.make_courses
     Course.all.each do |course|
+      if course.title && course.title != ""
+        puts "Title: #{course.title}"
+        puts "  Schedule: #{course.schedule}"
+        puts "  Description: #{course.description}"
+      end
+    end
+  end
+ 
 end
-end
-end
+ 
+Scraper.new.print_courses
 
-Scrapr.new.print_courses
+
